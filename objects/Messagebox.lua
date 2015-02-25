40abb024190624a211f9881f1a08164957ed319d
Kevin Nilsson <kevin_nilsson98@hotmail.com>
2015-02-25 17:35:43 +0100

    Update and rename objects to objects/Messagebox.lua
    Another placeholder! xP

---
+++ objects/Messagebox.lua
@@ -0,0 +1,29 @@
+--[[
+    [Object] Messagebox
+    @version 1.0, 2015-02-25
+    @author TheOddByte
+--]]
+
+
+
+local Messagebox = {}
+Messagebox.__index = Messagebox
+
+
+
+Messagebox.new = function( x, y, width, height, title, buttons )
+end
+
+
+
+function Messagebox:draw()
+end
+
+
+
+function Messagebox:handle( ... )
+end
+
+
+
+return Messagebox
