fea05134f1beb115a30e9f51544d7e970ffab5fd
Kevin Nilsson <kevin_nilsson98@hotmail.com>
2015-02-25 17:40:04 +0100

    Update and rename objects to objects/Button.lua

---
+++ objects/Button.lua
@@ -0,0 +1,29 @@
+--[[
+    [Object] Button
+    @version 1.0, 2015-02-25
+    @author TheOddByte
+--]]
+
+
+
+local Button = {}
+Button.__index = Button
+
+
+
+Button.new = function()
+end
+
+
+
+function Button:draw()
+end
+
+
+
+function Button:handle( ... )
+end
+
+
+
+return Button
