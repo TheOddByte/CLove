32ded58c371350ac6ee1ff40841b0ee990768b6d
Kevin Nilsson <kevin_nilsson98@hotmail.com>
2015-06-14 01:07:00 +0200

    Create Frame.lua
    Placeholder for now

---
+++ objects/Frame.lua
@@ -0,0 +1,20 @@
+--[[
+   [Object] Frame
+   @version 1.0, 2015-06-13
+   @author Kevin Nilsson
+--]]
+
+
+
+local Frame   = {}
+Frame.__index = Frame
+
+
+Frame.new = function() end
+
+
+
+
+function Frame:addObject( obj ) end
+
+function Frame:draw() end
